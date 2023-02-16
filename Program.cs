0d179b961dea3b51af644160739453adfb0a4eaa
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-02-16 11:26:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Tuple<string, int, int, int> tpl = new Tuple<string, int, int, int>("cica", 10, 20, 30);
+//(int, int) _tpl = (20, 30);
+//_tpl.Item1 = 40;
+//Console.WriteLine(_tpl.Item1 + " " + _tpl.Item2);
+
+(int X, int Y) pozition = (20, 30);
+
+Console.WriteLine(pozition.X);
+
+(string nev, int kor) ember = ("Zoli bácsi", 33);
+
+(string nev, int kor) Adatok(List<object> asd)
+{
+    return ("Béla", 21);
+}
